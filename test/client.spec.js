4da544ee15c4a90b39dd39cea1460044b2e6f347
Alexandr Voronkin <admin@elasticweb.ru>
2018-02-27 23:51:31 +0300

    auth

--- test/client.spec.js
+++ test/client.spec.js
@@ -19,16 +19,19 @@ describe('client', () => {
         await client.connect()
 
         const res = await client.ping()
-        assert.equal(res.head[0], 0)
-        assert.equal(res.head[5], 74)
+        console.log('res', res)
 
-        const res2 = await client.ping()
-        assert.equal(res2.head[0], 0)
-        assert.equal(res2.head[5], 74)
+        await client.close()
+    })
 
-        const res3 = await client.ping()
-        assert.equal(res3.head[0], 0)
-        assert.equal(res3.head[5], 74)
+    it('should auth', async () => {
+        const client = new Client()
+
+        await client.connect()
+
+        const res = await client.auth()
+
+        console.log('res', res)
 
         await client.close()
     })

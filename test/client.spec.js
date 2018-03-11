8877940b5b040df63722ba39fb147327ec6ad554
Alexandr Voronkin <admin@elasticweb.ru>
2018-03-11 18:12:51 +0300

    execute sql

--- test/client.spec.js
+++ test/client.spec.js
@@ -191,4 +191,31 @@ describe('client', () => {
 
         await client.close()
     })
+
+    describe('execute', () => {
+        it('should execute sql', async () => {
+            const client = new Client({ host, port})
+            await client.connect()
+
+            const result0 = await client.execute('DROP TABLE IF EXISTS table1')
+            assert.deepEqual(result0, { info: { row_count: 1 } })
+
+            const result1 = await client.execute('CREATE TABLE IF NOT EXISTS table1 (column1 INTEGER PRIMARY KEY, column2 VARCHAR(100))')
+            assert.deepEqual(result1, { info: { row_count: 1 } })
+
+            const result2 = await client.execute('INSERT INTO table1 VALUES (1, \'A\')')
+            assert.deepEqual(result2, { info: { row_count: 1 } })
+
+            const result3 = await client.execute('UPDATE table1 SET column2 = \'B\'')
+            assert.deepEqual(result3, { info: { row_count: 1 } })
+
+            const result4 = await client.execute('SELECT * FROM table1 WHERE column1 = 1')
+            assert.deepEqual(result4, {
+                data: [ [ 1, 'B' ] ],
+                metadata: [ { fieldName: 'column1' }, { fieldName: 'column2' } ]
+            })
+
+            await client.close()
+        })
+    })
 })
